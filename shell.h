9722003a2f840bef7fac1557930db9a415788df7
Allan Kamau <allanrittol@outlook.com>
2023-09-12 09:12:36 +0000

    getppid

---
+++ shell.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <syst/types.h>
+#include <syst/wait.h>
+#include <syst/stat.h>
+#include <limits.h>
+#include <fcntl.h>
+#include <errno.h>
+
+
+
+#endif
