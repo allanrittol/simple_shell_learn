9c867d4639d366cdb52dba3f87a1ac5123e2d4ca
Allan Kamau <allanrittol@outlook.com>
2023-09-14 06:03:24 +0000

    simpleshell

---
+++ get_line.c
@@ -0,0 +1,15 @@
+#include "shell.h"
+
+int main(void)
+{
+	size_t n = 10;
+	char *buf = NULL;
+
+	printf("Enter name:");
+	getline(&buf, &n, stdin);
+
+	printf("Name is %sBuffer size is %ld\n",buf, n);
+	free(buf);
+
+	return (0);
+}
