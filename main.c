cbb43479e1c352376626995ee6d9210ab3d80dba
Allan Kamau <allanrittol@outlook.com>
2023-09-14 08:13:30 +0000

    simpleshell

--- main.c
+++ main.c
@@ -1,4 +1,7 @@
 #include "main.h"
+#include <readline/readline.h>
+#include <readline/history.h>
+
 /**
  * main - main function for the simple shell
  * @ac: number of arguments
@@ -9,16 +12,18 @@
 int main(int ac, char **argv)
 {
         char *line = NULL;
-        char **command = NULL;
-        int status;
-        (void) ac;
 
         while (1)
         {
-                line = read_line();
-
-                command = tokenizer(line);
-
-                status = _execute(command, argv);
+                line = read_line(">> ");
+		
+		if (!line)
+		{
+			break;
+		}
+		add_history(line);
+		printf("You entered: %s\n", line);
+		free(line);
         }
+	return(0);
 }
